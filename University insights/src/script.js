776e9bf30d9693fcc316bde1ce1ae19a708d9817
Amrit12999 <129514564+Amrit12999@users.noreply.github.com>
2025-02-14 20:02:16 +0530

    Add files via upload

---
+++ University insights/src/script.js
@@ -0,0 +1,27 @@
+
+let icon = document.querySelector(".ri-menu-3-line")
+
+function menu(e){
+       
+
+        let list = document.querySelector("ul");
+        list.innerHTML =
+          ` <ul class="absolute top-[40px] left-[50%] bg-white h-[200px] w-[200px]">
+                                  <li class="py-5">
+                                      <a href="">Study Abroad</a>
+                                  </li>
+                                  <li class="">
+                                      <a href="">MBBS Abroad</a>
+                                  </li>
+                                  <li class="py-5">
+                                      <a href="">Resources</a>
+                                  </li>
+                                  <li>
+                                      <a href="contact.html">Contact Us</a>
+                                  </li>
+                               </ul>`
+    
+    
+       
+                               
+    }
